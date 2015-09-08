04b7877afc01b71ed9f869725a29ae3822d865c8
Vasil Vanchuk <v.vanchuk@tut.by>
2015-09-08 23:29:02 +0300

    Add assets

--- website/app.js
+++ website/app.js
@@ -1,12 +1,13 @@
 var koa = require('koa');
 var route = require('koa-route');
 var app = module.exports = koa();
+var serve = require('koa-static');
 
 var homeRotes = require('./routes/homeRoutes.js');
 
+app.use(serve(__dirname + '/public'));
+
 app.use(route.get('/', homeRotes.showHome));
-
-
 
 app.listen(3000);
 console.log("The app is listening on port 3000");
